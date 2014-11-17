e6efc94d090d42aaaaafd4e318aba046e6fc5e49
wotan <wotan.spark@gmail.com>
2014-11-18 05:04:14 +0900

    First commit. Add init.el and elisp/init-loader.el, .gitigore

---
+++ init.el
@@ -0,0 +1,21 @@
+;;; add-to-load-path function
+(defun add-to-load-path(&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory(expand-file-name(concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if 
+          (fboundp 'normal-top-level-add-subdirs-to-load-path)
+          (normal-top-level-add-subdirs-to-load-path)
+        )
+      )
+    )
+  )
+)
+
+;;; add load-path
+(add-to-load-path "server" "conf" "themes" "elisp" "elpa" "public_repos")
+
+;;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/conf")
