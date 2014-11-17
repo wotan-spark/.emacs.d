501ca585593cffcaa2c333995add7687c7daefa2
wotan <wotan.spark@gmail.com>
2014-11-18 06:50:56 +0900

    Setting Frame Title and Tool Bar, Menu Bar, Scroll Bar, Line Numbers, Highlight Current Line, Show Paren Mode

---
+++ conf/10-general.el
@@ -0,0 +1,21 @@
+;;; Frame Title
+(setq frame-title-format "%f")
+
+;;; Tool Bar
+(tool-bar-mode -1)
+
+;;; Menu Bar
+(menu-bar-mode t)
+
+;;; Scroll Bar
+(scroll-bar-mode 0)
+
+;;; Line Numbers
+(global-linum-mode t)
+
+;;; Highlight Current Line
+(global-hl-line-mode t)
+
+;;; Show Paren Mode
+(setq show-paren-delay 0)
+(show-paren-mode t)
