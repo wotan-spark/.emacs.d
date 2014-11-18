74ac4a1132785aa12ce57413ef6e94c2203957a2
wotan <wotan.spark@gmail.com>
2014-11-19 04:42:10 +0900

    Modified Show Paren Mode. changed show-paren-style and add set-face

--- conf/10-general.el
+++ conf/10-general.el
@@ -19,3 +19,7 @@
 ;;; Show Paren Mode
 (setq show-paren-delay 0)
 (show-paren-mode t)
+(setq show-paren-style 'expression)
+
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "#dc143c") ; crimson
