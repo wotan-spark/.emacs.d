cee776b7c3404f67e114912e44ae3fc393012110
wotan <wotan.spark@gmail.com>
2014-11-23 02:51:22 +0900

    Add PHP Mode

---
+++ conf/30-modes.el
@@ -0,0 +1,13 @@
+;;;; PHP Mode
+(when (require 'php-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.ctp\\'" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\'" . php-mode))
+  (setq php-search-url "http://jp.php.net/ja/")
+  (setq php-manual-url "http://jp.php.net/manual/ja/")
+  
+  (defun php-indent-hook()
+    (setq indent-tabs-mode nil)
+    (setq c-basic-offset 4)
+  )
+  (add-hook 'php-mode-hook 'php-indent-hook)
+)
