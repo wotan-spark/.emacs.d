1549c8baa1fdf17494477097ef96f52aeff21a30
wotan <wotan.spark@gmail.com>
2014-11-23 01:13:02 +0900

    Add Auto Install

--- conf/00-env.el
+++ conf/00-env.el
@@ -3,3 +3,10 @@
 
 ;;; Yes Or No P
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;;; Auto Install
+(require 'auto-install)
+(setq auto-install-directory "~/.emacs.d/elisp")
+(auto-install-update-emacswiki-package-name t)
+(auto-install-compatibility-setup)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
