27badafd5453fb9ffc0eb66161c5044770bc15bc
wotan <wotan.spark@gmail.com>
2015-01-13 16:44:32 +0900

    Modifed theme for DDSKK

--- themes/wotan-theme.el
+++ themes/wotan-theme.el
@@ -41,3 +41,17 @@
 (set-face-attribute 'whitespace-space nil :background nil :foreground "deepskyblue")
 (set-face-attribute 'whitespace-tab nil :background "gray20" :foreground "deepskyblue")
 (set-face-attribute 'whitespace-trailing nil :background "gray20" :foreground "deepskyblue")
+
+;;; DDSKK
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(skk-jisyo-edit-user-accepts-editing t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
