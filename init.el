1b6a1fd51aa09df7aa078cc891927e44e3545529
wotan <wotan.spark@gmail.com>
2015-01-13 19:03:09 +0900

    Add ELPA

--- init.el
+++ init.el
@@ -19,3 +19,14 @@
 ;;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/conf")
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
