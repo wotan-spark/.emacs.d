4c9d963043cb6b756287246dfcbc9a0575c2ca48
wotan <wotan.spark@gmail.com>
2015-01-18 00:53:49 +0900

    Setting Package add MELPA and Marmalade

--- init.el
+++ init.el
@@ -20,13 +20,8 @@
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/conf")
 
-
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
+;;; Package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
